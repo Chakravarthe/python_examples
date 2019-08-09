beeb8d7a2e8cc60dc4592a33e7c08bd1efb4afaa
Chakravarthe <48475319+Chakravarthe@users.noreply.github.com>
2019-08-09 14:25:53 +0530

    Add files via upload

---
+++ helllo.py
@@ -0,0 +1,12 @@
+#This program says to hello and asks for my name
+
+print('Welcome to my new world')
+print('what is your name')
+myName = input()
+print('it is good to meet you ' + myName)
+print('The length of your name in charecters is:')
+print(len(myName))
+print('what is your age')
+myAge = input()
+print('you will be' + str(int(myAge) + 1) + ' in a year')
+                          
